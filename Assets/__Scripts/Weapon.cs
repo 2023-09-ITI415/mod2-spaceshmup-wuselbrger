dd829eacbd622823779431d0ba6df119fc889934
wuselbrger <kevinkwu59@gmail.com>
2023-11-12 12:00:28 -0500

    created laser weapon type, adjusted weapon stats

--- Assets/__Scripts/Weapon.cs
+++ Assets/__Scripts/Weapon.cs
@@ -132,6 +132,12 @@ public class Weapon : MonoBehaviour {
                 p.transform.rotation = Quaternion.AngleAxis(-10, Vector3.back);
                 p.rigid.velocity = p.transform.rotation * vel;
                 break;
+            
+            case WeaponType.laser:
+                p = MakeProjectile();
+                p.rigid.velocity = vel;
+
+            break;
         }
     }
 
